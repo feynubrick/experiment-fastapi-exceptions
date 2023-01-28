1ce0af0cdd754cb03f551c719f5857ae937cdf8d
Seung-Yoon Oh <feynubrick@proton.me>
2023-01-28 21:23:56 +0900

    json response로 에러 보내고, 문서에도 표시하는 방법 추가

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -12,8 +14,24 @@ def root():
     "/error-http-exception",
 )
 def http_exception():
-    return HTTPException(
+    raise HTTPException(
         status_code=500,
         detail={"CUSTOM-ERROR-BODY-KEY": "VALUE"},
+        headers={"CUSTOM-HEADER": "VALUE"},
+    )
+
+
+# 위와 같이 하면 자동 생성되는 document에는 반영되지 않는다.
+# 에러를 문서에도 명시하려면 어떻게 해야하는가?
+# https://fastapi.tiangolo.com/advanced/additional-responses/
+class ErrorMessage(BaseModel):
+    message: str
+
+
+@app.get("/error-with-jsonresponse", responses={500: {"model": ErrorMessage}})
+def json_response():
+    return JSONResponse(
+        status_code=500,
+        content=ErrorMessage(message="Internal Server Error").json(),
         headers={"CUSTOM-HEADER": "VALUE"},
     )
