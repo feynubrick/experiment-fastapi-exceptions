8af43c847471107a8fccf345296366968b259034
Seung-Yoon Oh <feynubrick@proton.me>
2023-01-28 21:10:53 +0900

    HTTPException을 통해 에러응답 보내는 케이스 작성

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -6,3 +6,14 @@ app = FastAPI()
 @app.get("/")
 def root():
     return "HELLO WORLD!"
+
+
+@app.get(
+    "/error-http-exception",
+)
+def http_exception():
+    return HTTPException(
+        status_code=500,
+        detail={"CUSTOM-ERROR-BODY-KEY": "VALUE"},
+        headers={"CUSTOM-HEADER": "VALUE"},
+    )
